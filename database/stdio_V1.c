ba913aba9a414c7cea199c6ac4293b9a662c7907
Tanveer Alam <tanveeralam@Yori-MacBook-Air.local>
2025-11-21 19:35:17 -0500

    add database folder (30 file pairs)

---
+++ database/stdio_V1.c
@@ -0,0 +1,190 @@
+#include "headers/stdio.h"
+#include "../drivers/headers/uart.h"
+#include "../drivers/headers/sbi.h"
+
+long getc() {
+    return sbi_getc();
+}
+
+void putc(const char c) {
+    sbi_putc(c);
+}
+
+void puts(const char *str) {
+    while (*str != '\0') {
+        putc(*str);
+        str++;
+    }
+}
+
+void utoa(uint64_t value, char *buffer, int base) {
+    char *digits = "0123456789ABCDEF";
+    char temp[32];
+    int i = 0;
+
+    // Special case for zero
+    if (value == 0) {
+        buffer[0] = '0';
+        buffer[1] = '\0';
+        return;
+    }
+
+    // Convert the number to string
+    while (value > 0) {
+        temp[i++] = digits[value % base];
+        value /= base;
+    }
+
+    // Reverse the result into the final buffer
+    int j = 0;
+    while (i > 0) {
+        buffer[j++] = temp[--i];
+    }
+    buffer[j] = '\0'; // Terminate the string
+}
+
+void printf(char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
+
+void vprintf(const char *format, va_list args) {
+    char buffer[32];
+
+    while(*format) {
+        if (*format == '%') {
+            format++; // Skip this character
+            switch (*format) {
+                case 'd': { // Integer
+                    int value = va_arg(args, int);
+                    if (value < 0) {
+                        putc('-');
+                        value = -value;
+                    }
+                    utoa((uint64_t) value, buffer, 10);
+                    puts(buffer);
+                    break;
+                }
+
+                case 'x': { // Hexadecimal
+                    uint64_t value = va_arg(args, uint64_t);
+                    utoa(value, buffer, 16);
+                    puts(buffer);
+                    break;
+                }
+
+                case 'u': { // Unsigned Integer
+                    int value = va_arg(args, int);
+                    utoa((uint64_t) value, buffer, 10);
+                    puts(buffer);
+                    break;
+                }
+
+                case 's': { // String
+                    const char *str = va_arg(args, const char *);
+                    puts(str);
+                    break;
+                }
+
+                case 'c': { // Character
+                    char c = (char) va_arg(args, int);
+                    putc(c);
+                    break;
+                }
+
+                default: { // Unknown Format
+                    putc('%');
+                    putc(*format);
+                    break;
+                }
+            } 
+        } else {
+            // Newline should also carraige return
+            if (*format == '\n') {
+                putc('\r');
+            }
+            putc(*format);
+        }
+        format++;
+    }
+}
+
+void snprintf(char* buffer, size_t n, const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, n, format, args);
+    va_end(args);
+}
+
+void vsnprintf(char* buffer, size_t n, const char *format, va_list args) {
+    char temp_buffer[32];
+    uint32_t bufferIdx = 0;
+
+    while(*format) {
+        if (*format == '%') {
+            format++; // Skip this character
+            switch (*format) {
+                case 'd': { // Integer
+                    int value = va_arg(args, int);
+                    if (value < 0) {
+                        buffer[bufferIdx] = '-';
+                        bufferIdx++;
+                        value = -value;
+                    }
+                    utoa((uint64_t) value, temp_buffer, 10);
+                    buffer[bufferIdx] = *temp_buffer;
+                    bufferIdx += strlen(temp_buffer);
+                    break;
+                }
+
+                case 'u': {
+                    int value = va_arg(args, int);
+                    utoa((uint64_t) value, temp_buffer, 10);
+                    buffer[bufferIdx] = *temp_buffer;
+                    bufferIdx += strlen(temp_buffer);
+                    break;
+                }
+
+                case 'x': { // Hexadecimal
+                    unsigned int value = va_arg(args, unsigned int);
+                    utoa(value, temp_buffer, 16);
+                    buffer[bufferIdx] = *temp_buffer;
+                    bufferIdx += strlen(temp_buffer);
+                    break;
+                }
+
+                case 's': { // String
+                    const char *str = va_arg(args, const char *);
+                    buffer[bufferIdx] = *str;
+                    bufferIdx += strlen(str);
+                    break;
+                }
+
+                case 'c': { // Character
+                    char c = (char) va_arg(args, int);
+                    buffer[bufferIdx] = c;
+                    bufferIdx++;
+                    break;
+                }
+
+                default: { // Unknown Format
+                    putc('%');
+                    putc(*format);
+
+                    buffer[bufferIdx] = '%';
+                    bufferIdx++;
+                    buffer[bufferIdx] = *format;
+                    bufferIdx++;
+                    break;
+                }
+            } 
+        } else {
+            buffer[bufferIdx] = *format;
+            bufferIdx++;
+        }
+        format++;
+    }
+}
+
---
+++ database/virtio_blkio_V2.c
@@ -0,0 +1,98 @@
+/*
+    NOTE: This assumes only one blk_io device at a time!
+*/
+
+#include "virtio_blkio.h"
+#include "virtio.h"
+#include "../../klib/stdio.h"
+#include "../../klib/stdlib.h"
+#include "../../kernel/kernel.h"
+
+struct virt_queue *blk_request_vq;
+struct virtio_blk_req *blk_req;
+uint32_t blk_req_paddr; // May also be wrong because riscv32/riscv64
+uint64_t blk_capacity;
+
+#define port 0
+
+void virtio_blk_init(void) {
+    if (virtio_reg_read32(port, VIRTR_MAGIC_O) != VIRTIO_MAGIC_NUMBER) {
+        PANIC("virtio: invalid magic value!");
+    }
+    if (virtio_reg_read32(port, VIRTR_VERSION_O) != 1) {
+        PANIC("virtio: invalid version");
+    }
+    if (virtio_reg_read32(port, VIRTR_DEV_ID_O) != VIRTIO_DEVICE_BLK) {
+        PANIC("virtio: invalid device id");
+    }
+
+    // Reset device
+    virtio_reg_write32(port, VIRTR_DEV_STATUS_O, 0);
+    // Set ACK status bit
+    virtio_reg_fetch_and_or32(port, VIRTR_DEV_STATUS_O, VIRTIO_STATUS_ACKNOWLEDGE);
+    // Set DRIVER status bit
+    virtio_reg_fetch_and_or32(port, VIRTR_DEV_STATUS_O, VIRTIO_STATUS_DRIVER);
+    // Set FEATURES bit
+    virtio_reg_fetch_and_or32(port, VIRTR_DEV_STATUS_O, VIRTIO_STATUS_FEATURES_OK);
+    
+    // BLK device setup!
+    blk_request_vq = virtq_init(port, 0);
+    // Set the DRIVER_OK status bit
+    virtio_reg_write32(port, VIRTR_DEV_STATUS_O, VIRTIO_STATUS_DRIVER_OK);
+
+    // Get disk capacity
+    blk_capacity = virtio_reg_read64(port, VIRTR_CFG_O + 0) * SECTOR_SIZE;
+    printf("virtio-blk: capacity is %d bytes\n", blk_capacity);
+
+    // Allocate a region to store requests to the device.
+    blk_req_paddr = (uint32_t) kalloc(align_up(sizeof(*blk_req), PAGE_SIZE) / PAGE_SIZE);
+    if (!blk_req_paddr) {
+        PANIC("virtio-blk: out of memory");
+    }
+    blk_req = (struct virtio_blk_req *) blk_req_paddr;
+}
+
+void read_write_disk(void *buf, unsigned sector, int is_write) {
+    if (sector >= blk_capacity / SECTOR_SIZE) {
+        PANIC("virtio-blk: tried to read/write sector=%d, but capacity is %d", sector, blk_capacity / SECTOR_SIZE);
+        return;
+    }
+
+    // Build request
+    blk_req->sector = sector;
+    blk_req->type = is_write ? VIRTIO_BLK_T_OUT : VIRTIO_BLK_T_IN;
+    if (is_write) {
+        memcpy(blk_req->data, buf, SECTOR_SIZE);
+    }
+
+    // Build virtqueue descriptors
+    struct virt_queue *vq = blk_request_vq;
+    vq->buffers[0].addr = blk_req_paddr;
+    vq->buffers[0].len = sizeof(uint32_t) * 2 + sizeof(uint64_t); // sizeof(type, reserved, sector) in virtio_blk_req
+    vq->buffers[0].flags = VIRTQ_DESC_F_NEXT;
+    vq->buffers[0].next = 1;
+
+    vq->buffers[1].addr = blk_req_paddr + offsetof(struct virtio_blk_req, data);
+    vq->buffers[1].len = SECTOR_SIZE;
+    vq->buffers[1].flags = VIRTQ_DESC_F_NEXT | (is_write ? 0 : VIRTQ_DESC_F_WRITE);
+    vq->buffers[1].next = 2;
+
+    vq->buffers[2].addr = blk_req_paddr + offsetof(struct virtio_blk_req, status);
+    vq->buffers[2].len = sizeof(uint8_t);
+    vq->buffers[2].flags = VIRTQ_DESC_F_WRITE;
+
+    // Notify device of request
+    virtq_kick(port, vq, 0);
+
+    // Wait until the device finishes processing.
+    while (virtq_is_busy(vq)) {}
+
+    if (blk_req->status != 0) {
+        printf("virtio-blk: warn: failed to r/w sector=%d status%d\n", sector, blk_req->status);
+        return;
+    }
+
+    if (!is_write) {
+        memcpy(buf, blk_req->data, SECTOR_SIZE);
+    }
+}
