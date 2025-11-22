67ec138ca8781a9c0e5158e4134b0c5b10f331f9
Tanveer Alam <tanveeralam@Yori-MacBook-Air.local>
2025-11-21 19:25:46 -0500

    Remove files, will add again into a single folder

--- riscv_header_V2.h
+++
@@ -1,9 +0,0 @@
-#ifndef RISCV_H
-#define RISCV_H
-
-/*
-    RISCV specific stuff should USUALLY go here, but I only support this arch at the moment. When more archs are added,
-    this will be populated.
-*/
-
-#endif
--- trap_header_V1.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRAP_H
-#define TRAP_H
-
-#define SCAUSE_ECALL 8
-
-void init_trap_handler(void);
-
-#endif
